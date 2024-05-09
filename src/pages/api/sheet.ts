a65d2a2eb064db58b5f1b71275f0fb37b48eeb63
Luís Möllmann <luism6n@gmail.com>
2024-05-09 18:10:09 -0300

    retrieve json from spreadsheet

---
+++ src/pages/api/sheet.ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  sheet: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log("new request");
+
+  const sheetResponse = await fetch(
+    "https://docs.google.com/spreadsheets/d/1sKzh0Do_2fvIqhjwm-mNs5XaH6QOrvC-xTbYlnFg5bE/gviz/tq?tqx=out:json&sheet=PLANILHA"
+  );
+
+  if (sheetResponse.status !== 200) {
+    console.error(`failed to fetch sheet ${sheetResponse.body}`);
+    res.status(502);
+    return;
+  }
+
+  let jsonString: string;
+  try {
+    const regexGroups = /google.visualization.Query.setResponse\((.*)\);/.exec(
+      await sheetResponse.text()
+    );
+
+    if (!regexGroups?.length || regexGroups.length < 2) {
+      console.error(`failed to fetch sheet ${regexGroups}`);
+      res.status(500);
+      return;
+    } else {
+      jsonString = regexGroups[1];
+    }
+  } catch (error) {
+    console.error("error extracting JSON from response:", error);
+    res.status(500);
+    return;
+  }
+
+  res.send({ sheet: JSON.parse(jsonString) });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
+"use client";
+
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [data, setData] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/sheet");
+
+      if (res.status === 200) {
+        setData(await res.json());
+      } else {
+        setError(`${res.status} ${res.statusText}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!data) {
+    return <p>loading...</p>;
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h1>RSRS</h1>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
