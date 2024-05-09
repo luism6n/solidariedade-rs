3739573386f6d4a64c43981b735e22b708bc56c2
ntlchs <nataliachies@gmail.com>
2024-05-09 19:00:20 -0300

    fix head metatag error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* disable mobile zoom out */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
