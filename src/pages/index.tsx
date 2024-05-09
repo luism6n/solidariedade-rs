6e7d9beb7c0f8e23552d3c2b786ad59059fc7f55
ntlchs <nataliachies@gmail.com>
2024-05-09 19:00:56 -0300

    remove dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,24 +34,23 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-lg ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <header className="p-md bg-rose-700 w-full flex flex-col gap-lg">
+      <header className="p-lg bg-rose-700 w-full flex flex-col gap-lg">
         <div className="w-full flex justify-between items-center">
-          <div className="font-bold text-xl uppercase w-full">
+          <div className="font-bold text-xl uppercase w-full text-white">
             <h1>Solidariedade</h1>
             <h2 className="bg-gray-700 max-w-fit p-1 rounded-md">
               Rio Grande do Sul
             </h2>
           </div>
-          <PiInfo className="text-4xl" />
+          <PiInfo className="text-4xl" color="white" />
         </div>
         <div className="bg-white flex gap-md p-md items-center">
-          <input type="text" className="w-full text-black" />
-          <PiMagnifyingGlassBold className="text-2xl text-black" />
+          <input type="text" className="w-full" />
+          <PiMagnifyingGlassBold className="text-2xl" />
         </div>
       </header>
-      <h1>Site da Campanha Popular de Solidariedade do Rio Grande do Sul</h1>
 
       <pre className="max-w-md">{JSON.stringify(data, null, 2)}</pre>
     </main>
