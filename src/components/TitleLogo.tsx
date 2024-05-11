f0a3c100a14b3389f945e352fc5133bc1095a454
ntlchs <nataliachies@gmail.com>
2024-05-11 02:30:39 -0300

    make TitleLogo clickable

--- src/components/TitleLogo.tsx
+++ src/components/TitleLogo.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function TitleLogo() {
   return (
-    <div className="font-extrabold desktop:text-xl uppercase text-white">
+    <Link
+      href="/"
+      className="font-extrabold desktop:text-xl uppercase text-white"
+    >
       <h1 className="p-1">Campanha Popular de Solidariedade</h1>
       <h2 className="bg-stone-700 max-w-fit p-1 rounded-md">
         Rio Grande do Sul
       </h2>
-    </div>
+    </Link>
   );
 }
