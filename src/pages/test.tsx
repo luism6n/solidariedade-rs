4e7bccb31802a3c468b77d16c8908009bb296dd6
Luís Möllmann <luism6n@gmail.com>
2024-05-15 19:19:26 -0300

    clean up

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,9 +2,31 @@
 import MBPMap from "@/components/maps/MBPMap";
 import { Place, getGeocoding } from "@/components/maps/mapUtils";
 import { useScript } from "@/hooks/useScript";
+import { useEffect, useState } from "react";
 
 export default function Test() {
   const { isLoaded } = useScript({ name: "googleMaps" });
+
+  const [coordinates, setCoordinates] = useState<Place["position"] | null>(
+    null
+  );
+
+  const randomAddress =
+    "Av. Paulista, 1578 - Bela Vista, São Paulo - SP, 01310-200, Brazil";
+
+  useEffect(() => {
+    const init = async () => {
+      const { lat, lng } = await getGeocoding(randomAddress);
+
+      if (!lat || !lng) {
+        console.error("Erro ao converter endereço em coordenadas");
+        return;
+      }
+
+      setCoordinates({ lat, lng });
+    };
+    init();
+  }, []);
 
   const data: { places: Place[] } = {
     places: [
@@ -23,12 +45,14 @@ export default function Test() {
     ],
   };
 
-  const randomAddress =
-    "Av. Paulista, 1578 - Bela Vista, São Paulo - SP, 01310-200, Brazil";
-  const { lat, lng } = getGeocoding(randomAddress);
+  if (!coordinates || !isLoaded) {
+    return <p>Carregando...</p>;
+  }
+
+  const { lat, lng } = coordinates;
 
   return (
-    <div className="">
+    <>
       <h1>MAPA (test)</h1>
       <p>Test address: {randomAddress}</p>
       <p>
@@ -42,7 +66,7 @@ export default function Test() {
         )}
       </p>
 
-      {isLoaded ? <MBPMap data={data} /> : <p>Carregando Mapa...</p>}
-    </div>
+      <MBPMap data={data} />
+    </>
   );
 }
