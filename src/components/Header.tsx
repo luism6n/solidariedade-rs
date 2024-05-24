e1fc11cc13790c1ed46691c1f71797e061749813
Alan Tygel <alantygel@gmail.com>
2024-05-24 13:51:02 -0300

    Ajustes CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,10 +33,10 @@ export function Header({ children }: { children?: React.ReactNode }) {
                             <p className="mb-sm">Para isto, desenvolvemos esta iniciativa de mapeamento que tem como objetivo conectar àqueles com disponibilidade para apoiar, seja com força de trabalho ou doações, aos locais onde toda a ajuda será necessária a curto, médio e longo prazo. No momento, temos condições de contribuir na coordenação deste fluxo em Porto Alegre e em breve ampliaremos a organização para região metropolitana e interior.</p>
                             <p className="mb-sm">Criamos uma plataforma, com atualizações periódicas, capaz de filtrar as provisões necessárias de modo a fazer com que a vontade de ajudar do voluntário não esbarre no portão por falta de informação confiável. O diferencial do nosso trabalho de mobilização está no contato direto dos integrantes do nosso movimento social com os locais selecionados, sejam eles abrigos, cozinhas solidárias ou pontos de distribuição, de modo a ofertar informações confiáveis e atualizadas no que se refere às suas reais necessidades.</p>
                             <p className="mb-sm">Temos o compromisso ético e ideológico com o povo do nosso estado, com a certeza que a nossa iniciativa veio para ficar e que irá perdurar até que todos os desabrigados reencontrem o caminho da dignidade nos seus bairros, nas suas casas e nas suas vidas. Que seja plantada a semente da solidariedade, de forma que esta não fique restrita ao contexto das emergências, das crises e dos desastres, mas que floresça em um novo tempo: um amanhã solidário.</p>
-                            <p className="mb-sm font-bold">Realização:</p>
+                            <p className="mb-sm mt-lg font-bold">Realização:</p>
                             <div className="flex gap-lg">
                                 <Link href="https://brasilpopular.org"
-                                    className="font-extrabold uppercase text-white desktop:text-xl" target="_blank">
+                                    className="desktop:text-xl" target="_blank">
                                     <Image
                                         src="/mbp.webp"
                                         alt={"Movimento Brasil Popular"}
@@ -46,7 +46,7 @@ export function Header({ children }: { children?: React.ReactNode }) {
                                     />
                                 </Link>
                                 <Link href="https://eita.coop.br"
-                                    className="font-extrabold uppercase text-white desktop:text-xl" target="_blank">
+                                    className="mt-lg desktop:text-xl" target="_blank">
                                     <Image
                                         src="/EITA.png"
                                         alt={"Cooperativa EITA"}
@@ -56,10 +56,10 @@ export function Header({ children }: { children?: React.ReactNode }) {
                                     />
                                 </Link>
                             </div>
-                            <p className="mb-sm font-bold">Apoio:</p>
+                            <p className="mb-sm mt-lg font-bold">Apoio:</p>
                             <div className="flex gap-lg">
                                 <Link href="https://fiocruz.br"
-                                    className="font-extrabold uppercase text-white desktop:text-xl" target="_blank">
+                                    className="desktop:text-xl" target="_blank">
                                     <Image
                                         src="/Fiocruz.jpg"
                                         alt={"Fiocruz"}
@@ -69,6 +69,10 @@ export function Header({ children }: { children?: React.ReactNode }) {
                                     />
                                 </Link>
                             </div>
+                            <Link href="https://github.com/luism6n/solidariedade-rs/"
+                                className="desktop:text-xl" target="_blank">
+                                <p className="mt-lg mb-sm">Feito em Software Livre</p>
+                            </Link>
                         </div>
                     </Modal>
                 </div>
